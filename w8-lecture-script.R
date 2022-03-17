902da39e315e155c76209d36d02dd9aedd34cfb9
向一丹 <xiangyidan@macbook-pro.mynet>
2022-03-17 17:00:11 +0000

    use cor.test with a data frame to calculate p-value

--- w8-lecture-script.R
+++ w8-lecture-script.R
@@ -4,3 +4,7 @@ cor(x, y, method = 'pearson')
 cor(x, y, method = 'spearman')
 
 cor.test(x, y, method = 'pearson')
+
+fake.data <- data.frame(x = c(8,2,5,7,4,6,3,1), 
+                        y = c(622, 270, 466, 701, 124, 507, 382, 278))
+cor.test(~ x + y, method = 'pearson', data = fake.data)
