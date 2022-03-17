5c148873289d069d506d8bc48e8fa4554423f4c0
向一丹 <xiangyidan@macbook-pro.mynet>
2022-03-17 17:03:25 +0000

    using dplyr pipe to calculate p-value

--- w8-lecture-script.R
+++ w8-lecture-script.R
@@ -8,3 +8,7 @@ cor.test(x, y, method = 'pearson')
 fake.data <- data.frame(x = c(8,2,5,7,4,6,3,1), 
                         y = c(622, 270, 466, 701, 124, 507, 382, 278))
 cor.test(~ x + y, method = 'pearson', data = fake.data)
+
+library(dplyr)
+fake.data %>%
+  cor.test(~ x + y, method = 'pearson', data = .)
